abbe4e5503350590e900084e0a825ac42bc1b875
Lalithesh GP <lalithesh@neuralpath.in>
2026-03-24 22:31:15 +0530

    Debug: Log API Base URL to console

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '/api', headers: { 'Content-Type': 'application/json' } })
 
+console.log('🌐 API Base URL:', api.defaults.baseURL)
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken')
   if (token) config.headers.Authorization = `Bearer ${token}`
